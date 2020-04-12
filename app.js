7f0c90afd08e70be1208539eb28539a967795458
Thecodfun <46399816+Thecodfun@users.noreply.github.com>
2020-04-12 20:58:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -108,7 +108,8 @@ client.on('guildMemberAdd', member => {
           {
             message.reply("You **MUST** input a number of messages to delete!")
           }
-          message.channel.bulkDelete(args[0])  
+          message.bulkDelete(args[0]).then(() => {
+            message.channel.send(`Purged ${args[0]} messages!`).then(msg => msg.delete(3000));});  
           //message.delete();
           //const m_delnumber = args[2]({limit: 99});
           //message.channel.bulkDelete(fetched);
