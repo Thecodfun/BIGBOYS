98f6a256ff688fde0cc6217b718681ac62d816bc
Thecodfun <46399816+Thecodfun@users.noreply.github.com>
2020-04-12 19:38:38 +0200

    Gucci New Purge Command

--- app.js
+++ app.js
@@ -104,23 +104,18 @@ client.on('guildMemberAdd', member => {
       {
         if(message.member.hasPermission("MANAGE_MESSAGES" || "ADMINISTRATOR"))
         {
-          var user = message.mentions.members.first();
-          const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
-          if (!amount) return message.reply('Must specify an amount to delete!');
-          if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
-          message.channel.fetchMessages({
-          limit: 100,
-          }).then((messages) => {
-          if (user) {
-          const filterBy = user ? user.id : Client.user.id;
-          messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+          if(!agrs[0])
+          {
+            message.reply("You must to specify a number of messages to delete!")            
           }
-          message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
+          message.channel.bulkDelete(args[0]).then(() =>
+          {
+            message.channel.reply(`Purged ${args[0]} Messages!`).then(message => message.delete(3000))
           });
         }
         else
         {
-          message.reply(":x: You can't delete other user's messages! :x:")      
+          message.reply(":x: You can't delete messages! :x:")      
         }
       }
 
