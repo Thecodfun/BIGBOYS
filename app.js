7e120463781c40e92575ca33edf322eafb10e916
Thecodfun <46399816+Thecodfun@users.noreply.github.com>
2020-04-13 02:51:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@
       console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
       // Example of changing the bot's playing game to something useful. `client.user` is what the
       // docs refer to as the "ClientUser".
-      client.user.setActivity(`${client.guilds.size} Servers!`, { type: 'WATCHING' })
+      client.user.setActivity(`Big Boy Server's`, { type: 'WATCHING' })
     });
     
     client.on("message", async message => {
