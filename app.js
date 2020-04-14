7dc7f03770374a09692999ba05b8e1864da12e26
Thecodfun <46399816+Thecodfun@users.noreply.github.com>
2020-04-14 16:53:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@
     
     // Create an event listener for new guild members
      client.on("guildMemberAdd", (member) => {
-      client.channels.get('698863825121837079').send(`:partying_face: New User @${member.user.username} has joined our family, we hope you will enjoy your stay! :partying_face:`);
+      client.channels.get('698863825121837079').send(`:partying_face: New User ${member.user.tag} has joined our family, we hope you will enjoy your stay! :partying_face:`);
     });
 
     client.on("message", async message => {
