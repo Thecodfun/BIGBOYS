81f71b2a8458660840832d3e736883235fd49d42
Thecodfun <46399816+Thecodfun@users.noreply.github.com>
2020-04-14 21:33:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,6 @@
       const Discord = require("discord.js");
       const antispam = require('better-discord-antispam');
 
-      // This is your client. Some people call it `bot`, some people call it `self`, 
-      // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-      // this is what we're refering to. Your client.
       const client = new Discord.Client();  
       const config = require('./config.json');
       //PREFIX FOR COMANDS
@@ -31,31 +28,21 @@
         });
         // This event will run if the bot starts, and logs in, successfully.
         console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-        // Example of changing the bot's playing game to something useful. `client.user` is what the
-        // docs refer to as the "ClientUser".
+
         client.user.setActivity(`Big Boy's Server!`, { type: 'WATCHING' })
       });
       
-      // Create an event listener for new guild members
       client.on("guildMemberAdd", (member) => {
         client.channels.get('698863825121837079').send(`:partying_face: New User ${member} has joined our family, we hope you will enjoy your stay! :partying_face:`);
       });
 
       client.on("message", async message => {
-        // This event will run on every single message received, from any channel or DM.
 
-        // It's good practice to ignore other bots. This also makes your bot ignore itself
-        // and not get into a spam loop (we call that "botception").
         if(message.author.bot) return;
-        client.emit('checkMessage', message); // This runs the filter on any message bot receives in any guilds.
-        // Also good practice to ignore any message that does not start with our prefix, 
-        // which is set in the configuration file.
+        client.emit('checkMessage', message);
+
         if(message.content.indexOf(config.prefix) !== 0) return;
-        
-        // Here we separate our "command" name, and our "arguments" for the command. 
-        // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-        // command = say
-        // args = ["Is", "this", "the", "real", "life?"]
+
         const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
         const command = args.shift().toLowerCase();
 
@@ -76,8 +63,6 @@
 
         if(command === "ping") {
           if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send((":x: You can't ping the bot! :x:")).then(msg => msg.delete(2000))
-          // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-          // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
           const m = await message.channel.send("Ping?");
           m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
           }
@@ -196,7 +181,7 @@
                 }
                 message.channel.bulkDelete(messages).then( () => 
                 {
-                  message.channel.send(`Purged ${amount} of ${member}'s messages!`).then(msg => msg.delete(2000));
+                  message.channel.send(`Purged ${amount} of ${member}'s messages!`).then(msg => msg.delete(2000))
                 }).catch(error => console.log(error.stack));
               });
               
@@ -209,12 +194,12 @@
 
           if(command === "getid")
           {
-          { 
+            { 
             var member = message.mentions.members.first();
             var id = member.id.toString();
             if (!member) 
             {
-              message.reply(":x: You **MUST** specify a user to grab his ID! :x:").then(msg => msg.delete(2000));
+              message.reply(":x: You **MUST** specify a user to grab his ID! :x:").then(msg => msg.delete(2000))
             }
               message.reply("this is:" + member+"'s" + " id: " + id)}
           }
@@ -240,7 +225,7 @@
               " Those teeth look like you could eat an apple through a tennis racquet.", " No I'm not insulting you, I'm describing you.", " You're so fake, Barbie is jealous.", " I'd slap you, but that would be animal abuse.", "LOL RETARD."]
             if (!member) 
             {
-              message.reply(":x: You **MUST** specify a user to insult! :x:");
+              message.reply(":x: You **MUST** specify a user to insult! :x:").then(msg => msg.delete(2000))
             }
             else if( member.user.tag === botname)
             {
