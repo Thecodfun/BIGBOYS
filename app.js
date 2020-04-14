7b76949cfb50aeb1e918a58f1bfa8dee646c26f3
Thecodfun <46399816+Thecodfun@users.noreply.github.com>
2020-04-14 22:47:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,6 @@
       /////////////////////////////////////////
       client.on("guildMemberAdd", (member) => {
         client.channels.get('698863825121837079').send(`:partying_face: New User ${member} has joined our family, we hope you will enjoy your stay! :partying_face:`);
-        var role = message.guild.roles.get("699420767393153034");
         member.addRole(role);
       });
       /////////////////////////////////////////
