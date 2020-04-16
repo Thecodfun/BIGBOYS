34b1c7cb027f7f9be8054ebaddefb766f7503cda
Thecodfun <46399816+Thecodfun@users.noreply.github.com>
2020-04-16 20:29:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -224,7 +224,7 @@
             let Myembed = new Discord.RichEmbed ()
             .setAuthor("Big Boy's BOT", "https://i.imgur.com/dRbevw3.jpg")
             .setColor("#34cdeb")
-            .addField("Info about the BOT!", "This BOT is menat to help administrating the server and making evryone's life easier, Current Update is "+ version +",BOT is made in node.js using Discord.js APIs")
+            .addField("Info about the BOT!", "This BOT is meant to help administrating the server and making evryone's life easier, Current Update is "+ version +",BOT is made in node.js using Discord.js APIs")
             .setTimestamp()
             .setFooter("(BOT Made By 🤠₿ig ₿oy🤠#0549) || " + version)
             message.channel.send(Myembed)
@@ -270,14 +270,29 @@
         {
            message.reply("The coin you have inserted is not in the coinlist!")
         }
+        
         else
         {
+          //let apires = await CoinGeckoClient.simple.price({
+          //  ids:[coinname],
+          //  vs_currencies:a_vs_currencies,})
+
+          //  let response = ""
+          //  a_vs_currencies.forEach(cur => response += `\n${cur.toUpperCase()}: ${apires['data'][coinname][cur]}`)
+          //  message.reply(response)
+
           let apires = await CoinGeckoClient.simple.price({
             ids:[coinname],
             vs_currencies:a_vs_currencies,})
-
+  
             let response = ""
-            a_vs_currencies.forEach(cur => response += `\n${cur.toUpperCase()}: ${apires['data'][coinname][cur]}`)
+            let Myembed = new Discord.RichEmbed ()
+            .setAuthor("Big Boy's BOT", "https://i.imgur.com/dRbevw3.jpg")
+            .setColor("#0007cc")
+            message.channel.send(Myembed)
+            a_vs_currencies.forEach(cur => response += Myembed.addField("Coin Market Price Information", `${args[0].charAt(0).toUpperCase() + name.slice(1)} in ${cur.toUpperCase}: ${apires['data'][coinname][cur]}`)
+            .setTimestamp()
+            .setFooter("(BOT Made By 🤠₿ig ₿oy🤠#0549) || " + version))
             message.reply(response)
         }
       }
